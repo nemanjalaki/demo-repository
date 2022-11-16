795cf913b01031d1f6c8af3fcd0a93d85a074638
nemanjalaki <sone.lakicevic@gmail.com>
2022-11-16 19:33:11 +0100

    first commit on predavanje 1

---
+++ predavanje1/max_3_broja.py
@@ -0,0 +1,22 @@
+a = 500
+b = 501
+c = 485
+
+# if a>b:
+#     if a>c:
+#         print(a)
+#     else:
+#         print(c)
+# else:
+#     if b>c:
+#         print(b)
+#     else:
+#         print(c)
+
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
+
---
+++ predavanje1/suma_prvih_n.py
@@ -0,0 +1,29 @@
+# n = int(input("Unesi broj n: "))
+
+
+# suma = 0 
+# i = 1
+
+# while i <= n:
+#     suma+=i
+#     print("Suma", suma)
+#     i+=1
+#     print("Brojac", i)
+
+
+# for i in range(i,n+1):
+#     suma+=i
+#     print("Suma", suma)
+#     print("Brojac", i)
+    
+# print("Ukupna suma", suma)
+
+niz = [1,2,3,4, "test", "uvod", True]
+tekst = "nemanja"
+# for i in tekst:
+#     print(i)
+# print(tekst)
+tekst[0]="N"
+# print(tekst)
+# print(tekst[1])
+# print(niz)
